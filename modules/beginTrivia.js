de2a0580e574c6bc6372f1e097b6bb058b9e1cd0
Atlas2234 <zepro6966@gmail.com>
2024-05-26 10:37:51 -0400

    created a working game loop with accurate answer display

--- modules/beginTrivia.js
+++ modules/beginTrivia.js
@@ -1,5 +1,98 @@
-const beginTrivia = (gameList, gameContainer) => {
-console.log(gameList);
+// Delay the for loop so the display of the result can be read
+const sleep = async () => {
+  return new Promise(resolve => {
+    setTimeout(() => {resolve('')}, 3000);
+  });
+}
+
+const pauser = (stats) => {
+  
+  const right = document.querySelector('.rightAnswerBtn');
+  
+  const wrongs = document.querySelectorAll('.wrongAnswerBtn');
+
+  return new Promise(resolve => {
+  // Timer for a question
+  // Each question has max 30 seconds before the player has to answer
+  let count = 30;
+  // Countdown from 30
+  const timer = setInterval(function() {
+    // If the countdown is less than or equal to 0 then stop the interval
+    if(count <= 0) {
+      clearInterval(timer);
+      stats = 0;
+      // Return a resolve value of noTime
+      resolve("noTime");
+    } else {
+      // Display the number that is counted down
+      // Select the timer div
+      const headerTimer = document.querySelector('.timer');
+      // Add countdown progress bar
+      headerTimer.style.width = `${count * 1.5}vw`;
+    
+    }
+    count -= 1;
+  }, 1000);
+
+  // If the right answer is clicked
+  right.addEventListener('click', () => {
+    // Stop the countdown and unpause the for loop
+    stats = 0;
+    clearInterval(timer);
+    // Return a resolve value of correct
+    resolve("correct");
+  });
+
+  wrongs.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      console.log('hello');
+      stats = 0;
+      clearInterval(timer);
+      resolve("incorrect");
+    })
+  })
+
+  });
+}
+
+const beginTrivia = async (gameList, gameContainer) => {
+ // Loop through the questions
+ for (let i = 0; i < 10; i++) {
+  // Display one question
+  gameContainer.innerHTML = gameList[i];
+  // Pause the loop when a question starts
+  let stats = 1;
+  if (stats == 1) {
+    const prom = await pauser(stats);
+    console.log(prom);
+    // If the selected answer is correct
+    if(prom == 'correct') {
+      // Display the notification that the player was correct
+      gameContainer.innerHTML = `<h3 class='displayCorrect'>Correct!</h3>`;
+      // Delay the loop to display the message
+      await sleep();
+    // If the selected answer is wrong
+    } else if (prom == 'incorrect') {
+        // Display the notification that the player was wrong
+        // Show the player the right answer
+        const right = document.querySelector('.rightAnswerBtn');
+        gameContainer.innerHTML = `<h3 class='displayCorrect'>Incorrect</h3> <p>The correct answer is: ${right.textContent}</p>`;
+        // Delay the loop to display the message
+        await sleep();
+    // If the countdown reached 0
+    } else if (prom == 'noTime') {
+        // Display the notification that the time to answer was too long
+        // Show the player the right answer
+        const right = document.querySelector('.rightAnswerBtn');
+        gameContainer.innerHTML = `<h3 class='displayCorrect'>Time's Up</h3> <p>The correct answer was: ${right.textContent}</p>`;
+        // Delay the loop to display the message
+        await sleep();
+    }
+  }
+  // console.log(`the ${i}\'th iteration`);
+ }
+
+//  console.log(gameList);
 }
 
 export default beginTrivia;
--- modules/startPreparation.js
+++ modules/startPreparation.js
@@ -8,9 +8,11 @@ const startPrep = (data, title, container) => {
   // If the countdown is less than or equal to 0 then stop the interval and display the GO prompt
   if(count < 0) {
    clearInterval(timer);
+   // Get the trivia questions and answers
+   // Convert to array of HTML structures where each element is a question and their answers
    const gameArr = getDisplayGame(data);
+   // Start the main game loop
    beginGame(gameArr, container);
-   console.log(gameArr);
   } else if (count == 0) {
    title.innerHTML = `<h3>Go!</h3>`;
   } else {
