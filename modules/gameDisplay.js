d9eab6bed7c32f9bb58c746e90fcb449b4767196
Atlas2234 <zepro6966@gmail.com>
2024-05-30 15:45:04 -0400

    added loading

--- modules/gameDisplay.js
+++ modules/gameDisplay.js
@@ -1,6 +1,8 @@
 import startPrep from "./startPreparation.js";
 
 const gameDisplay = (data, title, container) => {
+ // Stop load display
+ container.classList.remove('loading');
  // Start the countdown to begin the trivia
  startPrep(data, title, container);
 }
